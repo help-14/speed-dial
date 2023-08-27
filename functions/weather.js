405d8c056b83904290483d61a55f0b780247aaf9
Help-14 <help-14@hotmail.com>
2023-08-27 17:46:06 +0700

    test lon, lat

--- functions/weather.js
+++ functions/weather.js
@@ -2,25 +2,32 @@ export async function onRequest({ request, env }) {
     const { searchParams } = new URL(request.url)
     let longitude = searchParams.get('longitude')
     let latitude = searchParams.get('latitude')
+    let response = { longitude, latitude }
 
-    if (!longitude || !latitude) return new Response({})
+    if (!longitude || !latitude) return new Response("Unknown location")
     const storageKey = `weather-${longitude}-${latitude}`
 
     const rawData = await env.API.get(storageKey);
     const weatherInfo = JSON.parse(rawData)
     const currentTime = new Date().getTime()
 
-    if (!weatherInfo?.data) return new Response({})
-
-    if (currentTime > weatherInfo.expireAt) {
-        const weatherResponse = {}
-        if (weatherResponse.ok) {
-            await env.API.put(storageKey, JSON.stringify({
-                expireAt: currentTime + 10 * 60 * 1000,
-                data: await weatherResponse.json()
-            }))
-            return new Response(weatherResponse)
+    if (weatherInfo?.data) {
+        response = weatherInfo.data
+        if (currentTime > weatherInfo.expireAt) {
+            const weatherResponse = {}
+            if (weatherResponse.ok) {
+                await env.API.put(storageKey, JSON.stringify({
+                    expireAt: currentTime + 10 * 60 * 1000,
+                    data: await weatherResponse.json()
+                }))
+                response = weatherResponse
+            }
         }
     }
-    return new Response(weatherInfo.data)
+
+    return new Response(JSON.stringify(response), {
+        headers: {
+            "content-type": "application/json;charset=UTF-8",
+        },
+    })
 }
