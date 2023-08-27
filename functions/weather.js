489aafaa0f542fd150fa8cb0c0ccccb1d99c90cc
Help-14 <help-14@hotmail.com>
2023-08-27 17:24:56 +0700

    test cf function kv

--- functions/weather.js
+++ functions/weather.js
@@ -1,3 +1,22 @@
-export function onRequest(context) {
-    return new Response("Hello, world!")
+export async function onRequest({ request, env }) {
+    const { searchParams } = new URL(request.url)
+    let longitude = searchParams.get('longitude')
+    let latitude = searchParams.get('latitude')
+    const storageKey = `weather-${longitude}-${latitude}`
+
+    const rawData = await env.API.get(storageKey);
+    const weatherInfo = JSON.parse(rawData)
+    const currentTime = new Date().getTime()
+
+    if (!weatherInfo || currentTime > weatherInfo.expireAt) {
+        const weatherResponse = {}
+        if (weatherResponse.ok) {
+            await env.API.set(storageKey, JSON.stringify({
+                expireAt: currentTime + 10 * 60 * 1000,
+                data: await weatherResponse.json()
+            }))
+            return weatherResponse
+        }
+    }
+    return new Response(weatherInfo.data)
 }
