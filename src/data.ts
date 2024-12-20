a2fcd433ed4053bfd1d8a02ed1bd3e8177e70f84
Nhan Phan <help-14@hotmail.com>
2024-12-20 13:32:09 +0700

    update unifi

--- src/data.ts
+++ src/data.ts
@@ -16,7 +16,7 @@ export const WebsiteData: WebData = {
             {
               title: "UniFi",
               icon: "fa-solid fa-u",
-              url: "http://192.168.0.1/network",
+              url: "http://10.0.0.1/network",
             },
             {
               title: "iHost",
